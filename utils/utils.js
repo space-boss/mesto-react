558868d0b90308d1c09eb72b321b18ba914fe3fa
Irina Herzberg <rirarum@yandex.com>
2021-03-23 21:34:06 +0100

    Updates

--- utils/utils.js
+++
@@ -1,9 +0,0 @@
-export function showLoading(button, loadingState) {
-  if (loadingState && button != null) {
-    button.value = "Сохранение...";
-  }
-  else {
-    button.value = "Сохранить";
-  }
-}
-
